585ece9e8702bf2e0e2df49bd5f64b32a73ddc4f
wkalevi1 <wkalevi@gmail.com>
2025-08-31 11:35:01 -0400

    Update utils.js

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,5 +1,6 @@
+// frontend/src/lib/utils.js
 import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
