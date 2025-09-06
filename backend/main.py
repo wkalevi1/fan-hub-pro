81884c75a507c88449dbfe4cf4774821db40a1c1
wkalevi1 <wkalevi@gmail.com>
2025-09-06 16:13:38 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/")
